030ab6fba83bda08f5d72a2ce81da23e0fca8765
Ryan O'Neill <ryanoneill416@gmail.com>
2023-02-06 16:12:29 +0000

    Implement PostDetail view automated testing

--- posts/tests.py
+++ posts/tests.py
@@ -37,3 +37,37 @@ class PostListViewTests(APITestCase):
             '/posts/',
             {'content': 'testing if unauthenticated users can post'})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class PostDetailViewTests(APITestCase):
+    def setUp(self):
+        nonartist = CustomUser.objects.create_user(
+            username='testuser', password='pass', is_artist=False)
+        artist = CustomUser.objects.create_user(
+            username='testartist', password='pass', is_artist=True)
+        Post.objects.create(
+            owner=artist, content='test post by artist'
+        )
+
+    def test_can_retrieve_post_using_valid_id(self):
+        response = self.client.get('/posts/1/')
+        self.assertEqual(response.data['content'], 'test post by artist')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_cant_retrieve_post_using_invalid_id(self):
+        response = self.client.get('/posts/2/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_artist_can_update_their_own_posts(self):
+        self.client.login(username='testartist', password='pass')
+        response = self.client.put(
+            '/posts/1/', {'content': 'test updating content'})
+        post = Post.objects.filter(pk=1).first()
+        self.assertEqual(post.content, 'test updating content')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_only_owner_can_update_the_post(self):
+        self.client.login(username='testuser', password='pass')
+        response = self.client.put(
+            '/posts/1/', {'content': 'This is not allowed for me to do'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
