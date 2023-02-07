34a876d2ebba93a20c2984877a71dec605656042
Ryan O'Neill <ryanoneill416@gmail.com>
2023-02-07 19:51:11 +0000

    Implement SaveList and SaveDetail generic views

--- saves/views.py
+++ saves/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from tatted_api.permissions import IsOwnerOrReadOnly
+from saves.models import Save
+from saves.serializers import SaveSerializer
 
-# Create your views here.
+
+class SaveList(generics.ListCreateAPIView):
+    """
+    List all save instances and like a post if authenticated user
+    """
+
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = SaveSerializer
+    queryset = Save.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class SaveDetail(generics.RetrieveDestroyAPIView):
+    """
+    Presents the save detail view.
+    Allows owners of the save instance to delete the instance
+    """
+
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = SaveSerializer
+    queryset = Save.objects.all()
