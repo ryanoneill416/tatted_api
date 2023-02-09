11e9ca5e3bb310c7a9e2b12417e066bf47df86af
Ryan O'Neill <ryanoneill416@gmail.com>
2023-02-09 18:56:28 +0000

    Create ReviewList and ReviewDetail views

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from tatted_api.permissions import IsOwnerOrReadOnly
+from .models import Review
+from .serializers import ReviewSerializer, ReviewDetailSerializer
 
-# Create your views here.
+
+class ReviewList(generics.ListCreateAPIView):
+    serializer_class = ReviewSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Review.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ReviewDetailSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Review.objects.all()
