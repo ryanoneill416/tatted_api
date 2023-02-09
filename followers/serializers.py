b32db55dd2a673e34217354dbf83ac45e50dfbde
Ryan O'Neill <ryanoneill416@gmail.com>
2023-02-09 14:48:54 +0000

    Create FollowerSerializer

---
+++ followers/serializers.py
@@ -0,0 +1,25 @@
+from django.db import IntegrityError
+from rest_framework import serializers
+from .models import Follower
+
+
+class FollowerSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Follower model
+    Create method ensures a unique relationship between 'owner' and 'followed'
+    """
+
+    owner = serializers.ReadOnlyField(source='owner.username')
+    followed_username = serializers.ReadOnlyField(source='followed.username')
+
+    class Meta:
+        model = Follower
+        fields = [
+            'id', 'created_at', 'owner', 'followed', 'followed_username'
+        ]
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializer.ValidationError({'detail': 'possible duplicate'})
