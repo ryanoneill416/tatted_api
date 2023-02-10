bd93ff00ef333238207d5e53f5aed7db718bea72
Ryan O'Neill <ryanoneill416@gmail.com>
2023-02-10 16:15:08 +0000

    Add another filter of CommentList view to sort comments by post they are related to

--- comments/views.py
+++ comments/views.py
@@ -1,4 +1,6 @@
+from django.db.models import Count
 from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 from tatted_api.permissions import IsOwnerOrReadOnly
 from .models import Comment
 from .serializers import CommentSerializer, CommentDetailSerializer
@@ -8,6 +10,12 @@ class CommentList(generics.ListCreateAPIView):
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Comment.objects.all()
+    filter_backends = [
+        DjangoFilterBackend
+    ]
+    filterset_fields = [
+        'post'
+    ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
