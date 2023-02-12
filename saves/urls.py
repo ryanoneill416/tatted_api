313a433589e0ba2425cca2f62829506632e29782
Ryan O'Neill <ryanoneill416@gmail.com>
2023-02-12 18:51:43 +0000

    Set up JWT tokens and customuserserializer in preparation to deplot

--- saves/urls.py
+++ saves/urls.py
@@ -3,5 +3,5 @@ from saves import views
 
 urlpatterns = [
     path('saves/', views.SaveList.as_view()),
-    path('saves/<int:pk>', views.SaveDetail.as_view()),
+    path('saves/<int:pk>/', views.SaveDetail.as_view()),
 ]
--- tatted_api/settings.py
+++ tatted_api/settings.py
@@ -27,6 +27,20 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [(
+        'rest_framework.authentication.SessionAuthentication'
+        if 'DEV' in os.environ
+        else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+    )]
+}
+
+REST_USE_JWT = True
+JWT_AUTH_SECURE = True
+JWT_AUTH_COOKIE = 'my-app-auth'
+JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -44,7 +58,7 @@ SITE_ID = 1
 AUTH_USER_MODEL = 'users.CustomUser'
 
 REST_AUTH_SERIALIZERS = {
-    'USER_DETAILS_SERIALIZER': 'users.serializers.UserSerializer',
+    'USER_DETAILS_SERIALIZER': 'tatted_api.serializers.CurrentUserSerializer'
 }
 
 # Application definition
@@ -60,11 +74,11 @@ INSTALLED_APPS = [
     'cloudinary',
     'rest_framework',
     'rest_framework.authtoken',
-    'rest_auth',
+    'dj_rest_auth',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
-    'rest_auth.registration',
+    'dj_rest_auth.registration',
     'django_filters',
 
     'users',
