65554eaa15f84603b155744185b26ddd34f8e0f3
Ryan O'Neill <ryanoneill416@gmail.com>
2023-02-12 19:06:10 +0000

    Add finishing touches to API including root route, datetime formatting and pagination

--- tatted_api/urls.py
+++ tatted_api/urls.py
@@ -1,8 +1,9 @@
-from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+from .views import root_route
 
 urlpatterns = [
+    path('', root_route),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
