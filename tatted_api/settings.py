c44a71bd1d1440ae40057dcd244422a6f7fc8038
Ryan O'Neill <ryanoneill416@gmail.com>
2023-02-12 19:38:12 +0000

    Adjust allowed_hosts heroku name

--- tatted_api/settings.py
+++ tatted_api/settings.py
@@ -62,7 +62,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'tatted.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'tatted-api.herokuapp.com']
 
 
 SITE_ID = 1
