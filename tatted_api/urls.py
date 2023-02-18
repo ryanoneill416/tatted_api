95a76881d20e8ba707c70785778c13874d24734f
Ryan O'Neill <ryanoneill416@gmail.com>
2023-02-18 12:48:32 +0000

    remove default registration url no longer being used

--- tatted_api/urls.py
+++ tatted_api/urls.py
@@ -9,9 +9,6 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('dj-rest-auth/logout/', logout_route),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path(
-        'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
-    ),
     path('', include('profiles.urls')),
     path('', include('users.urls')),
     path('', include('posts.urls')),
