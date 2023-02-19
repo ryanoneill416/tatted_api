b117e5b9218f7356b78f2d7a993277fc3a775e93
Ryan O'Neill <ryanoneill416@gmail.com>
2023-02-19 20:36:29 +0000

    adjust default image upon user creation to have a transparent background to improve frontend UI

--- profiles/models.py
+++ profiles/models.py
@@ -11,7 +11,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=255, blank=True)
     bio = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_profile_jqu1gh'
+        upload_to='images/', default='../drilldown-removebg-preview_dxh3rz'
     )
 
     class Meta:
