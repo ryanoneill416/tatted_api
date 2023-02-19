3c7b9873cd5bb76e343befc13e0a7c8f6afe39f2
Ryan O'Neill <ryanoneill416@gmail.com>
2023-02-19 20:09:50 +0000

    Update CurrentUserSerializer to withhold the is_artist field for referencing in the frontend

--- tatted_api/serializers.py
+++ tatted_api/serializers.py
@@ -5,8 +5,9 @@ from rest_framework import serializers
 class CurrentUserSerializer(UserDetailsSerializer):
     profile_id = serializers.ReadOnlyField(source='profile.id')
     profile_image = serializers.ReadOnlyField(source='profile.image.url')
+    is_artist = serializers.ReadOnlyField(source='profile.owner.is_artist')
 
     class Meta(UserDetailsSerializer.Meta):
         fields = UserDetailsSerializer.Meta.fields + (
-            'profile_id', 'profile_image'
+            'profile_id', 'profile_image', 'is_artist',
         )
